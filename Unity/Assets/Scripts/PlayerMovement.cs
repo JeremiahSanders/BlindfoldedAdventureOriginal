2f272065257cddd66d4af692e61f858fa7b9dbd8
Jeremiah Sanders <blujai@blujai.com>
2015-01-24 10:34:03 -0600

    Added character controller package; added initial movement script

---
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public int MovementSpeed = 1;
+    // Use this for initialization
+    private void Start() {}
+    // Update is called once per frame
+    private void Update()
+    {
+        float vert = Input.GetAxis("Vertical") * MovementSpeed;
+        float hor = Input.GetAxis("Horizontal") * MovementSpeed;
+        vert *= Time.deltaTime;
+        hor *= Time.deltaTime;
+        transform.Translate(hor, 0, vert);
+        //transform.Rotate(0, rotation, 0);
+    }
+}
